d4fddfb3c374936ae3d08400be8c5de3754e5ec9
prathyusha1311 <prathyusha.nareshkumar2001@gmail.com>
2024-12-09 05:04:55 -0800

    Second commit with changes to the typical

--- src/PortfolioContainer/Home/Profile/Profile.js
+++ src/PortfolioContainer/Home/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Typical from "react-typical";
+import Typewriter from "typewriter-effect";
 import "./Profile.css";
 
 export default function Profile() {
@@ -9,12 +9,6 @@ export default function Profile() {
         <div className="profile-details">
           <div className="colz">
             <div className="colz-icon">
-              {/* <a href="#">
-                <i className="fa fa-facebook-square"></i>
-              </a>
-              <a href="#">
-                <i className="fa fa-google-plus-square"></i>
-              </a> */}
               <a href="#">
                 <i className="fa fa-instagram"></i>
               </a>
@@ -29,51 +23,62 @@ export default function Profile() {
           <div className="profile-details-name">
             <span className="primary-text">
               {" "}
-              Hello, I'm <span className="highlighted-text"> Prathyusha!</span>
+              Hello, I'm <span className="highlighted-text">Prathyusha!</span>
             </span>
           </div>
 
           <div className="profile-details-role">
             <span className="primary-text">
-              {" "}
               <h1>
-                {" "}
-                <Typical
-                  loop={Infinity}
-                  steps={[
-                    "Software Engineer", 3000, // Type and wait 2 seconds
-                    "Machine Learning Engineer", 3000,
-                    "Natural Language Processing Engineer", 3000,
-                    "Computer Vision Enthusiast", 3000,
-                    "AI Engineer", 3000,
-                    "Data Scientist", 3000,
-                    "Full Stack Web Application Developer", 3000,
-                  ]}
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Software Engineer",
+                      "Machine Learning Engineer",
+                      "Natural Language Processing Engineer",
+                      "Computer Vision Enthusiast",
+                      "AI Engineer",
+                      "Data Scientist",
+                      "Full Stack Web Application Developer",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    delay: 50, // Delay between each character
+                    deleteSpeed: 30, // Speed of deleting characters
+                  }}
                 />
               </h1>
               <span className="profile-role-tagline">
-              Blending research expertise with technical prowess to create innovative data-driven solutions, <br/>leveraging Machine Learning and AI for impactful web and mobile applications.
+                Blending research expertise with technical prowess to create
+                innovative data-driven solutions, <br/>leveraging Machine Learning
+                and AI for impactful web and mobile applications.
               </span>
             </span>
           </div>
           <div className="profile-options">
-            <button className="btn primary-btn"
-            onClick={() => {
-              const contactMeSection = document.getElementById("ContactMe");
-              if (contactMeSection) {
-                contactMeSection.scrollIntoView({ behavior: "smooth" });
-              }
-            }}
-            > Hire Me </button>
-            <a href="https://drive.google.com/file/d/1DFZx9famKjQwwlcvohZ86vPV-sqjEZxz/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-  <button className="btn highlighted-btn">Get Resume</button>
-</a>
+            <button
+              className="btn primary-btn"
+              onClick={() => {
+                const contactMeSection = document.getElementById("ContactMe");
+                if (contactMeSection) {
+                  contactMeSection.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              Hire Me
+            </button>
+            <a
+              href="https://drive.google.com/file/d/1DFZx9famKjQwwlcvohZ86vPV-sqjEZxz/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
           </div>
-          
         </div>
         <div className="profile-picture">
-            <div className="profile-picture-background"></div>
-          </div>
+          <div className="profile-picture-background"></div>
+        </div>
       </div>
     </div>
   );
